f02b766c0d05277349b2608ed0691abe7805fb86
ductnn <trannhuduc531998@gmail.com>
2021-07-01 10:59:44 +0700

    Fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const port = process.env.PORT || PORT_START;
+const port = process.env.PORT || 3000;
 const ngrok = require('ngrok');
 const express = require('express');
 const bodyParser = require('body-parser');
